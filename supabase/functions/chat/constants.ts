7a40e5d45408531850d7cca855189989705184c3
chandan-m <chandanmanjunath24@gmail.com>
2025-04-18 13:49:47 -0700

    .

--- supabase/functions/chat/constants.ts
+++ supabase/functions/chat/constants.ts
@@ -2,5 +2,4 @@
 export const GPT_MODEL = "gpt-3.5-turbo";
 export const GPT_MODEL_BEHAVIOUR = "You are a helpful assistant that chats naturally with users";
 
-export const MAX_CHAT_HISTORY_CONTEXT = 4;
-
+export const MAX_CHAT_HISTORY_CONTEXT = 4; // Controls the chat context sent to the model with every request
