815a3b22eff6d1e25642210e6a375f145119599c
chandan-m <chandanmanjunath24@gmail.com>
2025-04-18 21:58:07 -0700

    .

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -145,7 +145,7 @@ const Chat: React.FC<Props> = ({userId, setUserId}) => {
                     className="bg-orange-400 text-white rounded-full px-4 py-2 font-bold hover:bg-orange-500 transition"
                     disabled={loading}
                 >
-                    Sending
+                    Send
                 </button>
             </form>
         </div>
